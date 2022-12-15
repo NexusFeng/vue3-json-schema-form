52c14ee0cd516f16874850c8efca58b3f0b5c81f
NexusFeng <1622920569@qq.com>
2022-12-15 23:27:45 +0800

    udp: '初始化'

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { defineComponent, reactive, ref, Ref } from 'vue'
+import MonacoEditor from './components/MonacoEditor'
+
+function toJson(data: any) {
+  return JSON.stringify(data, null, 2)
+}
+
+const schema = {
+  type: 'string',
+}
+
+export default defineComponent({
+  setup() {
+    const schemaRef: Ref<any> = ref(schema)
+    const handleCodeChange = (code: string) => {
+      let schema: any
+      try {
+        schema = JSON.parse(code)
+      } catch (err) {
+        schemaRef.value = schema
+      }
+    }
+
+    return () => {
+      const code = toJson(schemaRef.value)
+
+      return (
+        <div>
+          <MonacoEditor
+            code={code}
+            onChange={handleCodeChange}
+            title="schema"
+          ></MonacoEditor>
+        </div>
+      )
+    }
+  },
+})
