3d9c6751c3425bd7077c2f15a942605f33bac418
NexusFeng <1622920569@qq.com>
2022-12-18 21:48:43 +0800

    udp: '组件初始化'

--- src/components/MonacoEditor.tsx
+++ src/components/MonacoEditor.tsx
@@ -1,14 +1,8 @@
-import {
-  defineComponent,
-  ref,
-  onMounted,
-  watch,
-  onBeforeUnmount,
-  shallowRef,
-} from 'vue'
+/* eslint-disable */
+
+import { defineComponent, ref, onMounted, watch, onBeforeUnmount, shallowRef } from 'vue'
 
 import * as Monaco from 'monaco-editor'
-
 import type { PropType } from 'vue'
 import { createUseStyles } from 'vue-jss'
 
@@ -17,7 +11,7 @@ const useStyles = createUseStyles({
     border: '1px solid #eee',
     display: 'flex',
     flexDirection: 'column',
-    borderRadius: 5,
+    borderRadius: 5
   },
   title: {
     backgroundColor: '#eee',
@@ -26,97 +20,93 @@ const useStyles = createUseStyles({
   },
   code: {
     flexGrow: 1,
-  },
+  }
 })
 
 export default defineComponent({
   props: {
     code: {
       type: String as PropType<string>,
-      required: true,
+      required: true
     },
     onChange: {
-      type: Function as PropType<
-        (value: string, event: Monaco.editor.IModelContentChangedEvent) => void
-      >,
-      required: true,
+      type: Function as PropType<(value: string, event: Monaco.editor.IModelContentChangedEvent) => void>,
+      required: true
     },
     title: {
       type: String as PropType<string>,
-      required: true,
-    },
+      required: true
+    }
   },
   setup(props) {
+    // must be shallowRef, if not, editor.getValue() won't work
     const editorRef = shallowRef()
+
     const containerRef = ref()
 
     let _subscription: Monaco.IDisposable | undefined
     let __prevent_trigger_change_event = false
 
     onMounted(() => {
-      const editor = (editorRef.value = Monaco.editor.create(
-        containerRef.value,
-        {
-          value: props.code,
-          language: 'json',
-          formatOnPaste: true,
-          tabSize: 2,
-          minimap: {
-            enabled: false,
-          },
-        }
-      ))
+      const editor = editorRef.value = Monaco.editor.create(containerRef.value, {
+        value: props.code,
+        language: 'json',
+        formatOnPaste: true,
+        tabSize: 2,
+        minimap: {
+          enabled: false,
+        },
+      })
 
       _subscription = editor.onDidChangeModelContent((event) => {
+        console.log('--------->', __prevent_trigger_change_event)
         if (!__prevent_trigger_change_event) {
-          props.onChange(editor.getValue(), event)
+          props.onChange(editor.getValue(), event);
         }
-      })
+      });
     })
 
     onBeforeUnmount(() => {
-      if (_subscription) {
+      if (_subscription)
         _subscription.dispose()
-      }
     })
 
-    watch(
-      () => props.code,
-      (v) => {
-        const editor = editorRef.value
-        const model = editor.getModel()
-
-        if (v !== model.getValue()) {
-          editor.pushUndoStop()
-          __prevent_trigger_change_event = true
-          model.pushEditOperations(
-            [],
-            [
-              {
-                range: model.getFullModeRange(),
-                text: v,
-              },
-            ]
-          )
-          editor.pushUndoStop()
-          __prevent_trigger_change_event = false
-        }
+    watch(() => props.code, (v) => {
+      const editor = editorRef.value
+      const model = editor.getModel()
+      if (v !== model.getValue()) {
+        editor.pushUndoStop();
+        __prevent_trigger_change_event = true
+        // pushEditOperations says it expects a cursorComputer, but doesn't seem to need one.
+        model.pushEditOperations(
+          [],
+          [
+            {
+              range: model.getFullModelRange(),
+              text: v,
+            },
+          ]
+        );
+        editor.pushUndoStop();
+        __prevent_trigger_change_event = false
       }
-    )
+      // if (v !== editorRef.value.getValue()) {
+      //   editorRef.value.setValue(v)
+      // }
+    })
 
     const classesRef = useStyles()
 
     return () => {
+
       const classes = classesRef.value
 
       return (
         <div class={classes.container}>
-          <div class={classes.title}>
-            <span>{props.title}</span>
-          </div>
+          <div class={classes.title}><span>{props.title}</span></div>
           <div class={classes.code} ref={containerRef}></div>
         </div>
       )
     }
-  },
+  }
 })
